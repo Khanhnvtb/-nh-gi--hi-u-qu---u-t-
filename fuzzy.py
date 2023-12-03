70298912226797ec53315623bd62ce5bfdb9bbc5
khanhnvtb <khanhtb.1012@gmail.com>
2023-12-03 09:40:56 +0700

    Add all untracked files

---
+++ fuzzy.py
@@ -0,0 +1,120 @@
+class RoleFunction:
+    def __init__(self):
+        self.OHQ = 0
+        self.KHQ = 0
+        self.HQ = 0
+
+class Fuzzy_Role_Base:
+    """
+    self.index_profit : {
+        0: 'TST',
+        1: 'TSTB',
+        2: 'TSC'
+    }
+    self.index_cost : {
+        0: 'CPT',
+        1: 'CPTB',
+        2: 'CPC'
+    }
+    self.index_time : {
+        0: 'TGN',
+        1: 'TGTB',
+        2: 'TGD'
+    }
+    self.label : {
+        0: 'OHQ',
+        1: 'KHQ',
+        2: 'HQ'
+    }
+    """
+    def __init__(self, index_profit, index_cost, index_time, label):
+        self.index_profit = index_profit
+        self.index_cost = index_cost
+        self.index_time = index_time
+        self.label = label
+
+    def update_role_function(self, fuzzy):
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    if fuzzy.μ_profit[i] and self.index_profit == i and fuzzy.μ_cost[j] and self.index_cost == j and fuzzy.μ_time[k] and self.index_time == k:
+                        value = min(fuzzy.μ_profit[i], fuzzy.μ_cost[j], fuzzy.μ_time[k])
+                        if self.label == 0:
+                            fuzzy.role_function.OHQ = max(fuzzy.role_function.OHQ, value)
+                        elif self.label == 1:
+                            fuzzy.role_function.KHQ = max(fuzzy.role_function.KHQ, value)
+                        else:
+                            fuzzy.role_function.HQ = max(fuzzy.role_function.HQ, value)
+
+class Fuzzy:
+    def __init__(self, profit, cost, time):
+        self.μ_profit = self._fuzzifier_profit(profit)
+        self.μ_cost = self._fuzzifier_cost(cost)
+        self.μ_time = self._fuzzifier_time(time)
+        self.fuzzy_role_base = [
+            Fuzzy_Role_Base(0, 0, 0, 1),
+            Fuzzy_Role_Base(0, 0, 1, 1),
+            Fuzzy_Role_Base(0, 0, 2, 0),
+            Fuzzy_Role_Base(0, 1, 0, 1),
+            Fuzzy_Role_Base(0, 1, 1, 0),
+            Fuzzy_Role_Base(0, 1, 2, 0),
+            Fuzzy_Role_Base(0, 2, 0, 0),
+            Fuzzy_Role_Base(0, 2, 1, 0),
+            Fuzzy_Role_Base(0, 2, 2, 0),
+            Fuzzy_Role_Base(1, 0, 0, 2),
+            Fuzzy_Role_Base(1, 0, 1, 2),
+            Fuzzy_Role_Base(1, 0, 2, 1),
+            Fuzzy_Role_Base(1, 1, 0, 2),
+            Fuzzy_Role_Base(1, 1, 1, 1),
+            Fuzzy_Role_Base(1, 1, 2, 0),
+            Fuzzy_Role_Base(1, 2, 0, 1),
+            Fuzzy_Role_Base(1, 2, 1, 0),
+            Fuzzy_Role_Base(1, 2, 2, 0),
+            Fuzzy_Role_Base(2, 0, 0, 2),
+            Fuzzy_Role_Base(2, 0, 1, 2),
+            Fuzzy_Role_Base(2, 0, 2, 2),
+            Fuzzy_Role_Base(2, 1, 0, 2),
+            Fuzzy_Role_Base(2, 1, 1, 2),
+            Fuzzy_Role_Base(2, 1, 2, 1),
+            Fuzzy_Role_Base(2, 2, 0, 2),
+            Fuzzy_Role_Base(2, 2, 1, 1),
+            Fuzzy_Role_Base(2, 2, 2, 1),
+        ]
+        
+        self.role_function = None
+        self.investment_index = None
+
+    @staticmethod
+    def _fuzzifier_profit(profit):
+        μ_low = lambda x: 1 if x < 20 else ((60 - x) / (60 - 20)) if 20 <= x <= 60 else 0
+        μ_medium = lambda x: 0 if x < 0 else (x / 30) if 0 <= x < 30 else 1 if 30 <= x < 50 else ((100 - x) / (100 - 50)) if 50 <= x <= 100 else 0
+        μ_high = lambda x: 0 if x < 20 else (x - 20) / (60 - 20) if 20 <= x <= 60 else 1
+        return (μ_low(profit), μ_medium(profit), μ_high(profit))
+
+    @staticmethod
+    def _fuzzifier_cost(cost):
+        μ_low = lambda x: 1 if x < 30 else (500 - x) / (500 - 30) if 30 <= x <= 500 else 0
+        μ_medium = lambda x: 0 if x < 30 else (x - 30) / (100 - 30) if 30 <= x < 100 else 1 if 100 <= x < 150 else (500 - x) / (500 - 150) if 150 <= x <= 500 else 0
+        μ_high = lambda x: 0 if x < 65 else (x - 65) / (500 - 65) if 65 <= x <= 500 else 1
+        return (μ_low(cost), μ_medium(cost), μ_high(cost))
+
+    @staticmethod
+    def _fuzzifier_time(time):
+        μ_short = lambda x: 1 if x < 3 else (18 - x) / (18 - 3) if 3 <= x <= 18 else 0
+        μ_medium = lambda x: 0 if x < 3 else (x - 3) / (6 - 3) if 3 <= x < 6 else 1 if 6 <= x < 9 else (18 - x) / (18 - 9) if 9 <= x <= 18 else 0
+        μ_long = lambda x: 0 if x < 3 else (x - 3) / (18 - 3) if 3 <= x <= 18 else 1
+        return (μ_short(time), μ_medium(time), μ_long(time))
+    
+    def inference(self):
+        self.role_function = RoleFunction()
+        for fuzzy_role_base in self.fuzzy_role_base:
+            fuzzy_role_base.update_role_function(self)
+    
+    def defuzzifier(self):
+        if not self.role_function or self.role_function == RoleFunction():
+            self.investment_index = 0
+        else:
+            self.investment_index = (self.role_function.OHQ * 2 + self.role_function.KHQ * 4 + self.role_function.HQ * 7.5) / (self.role_function.OHQ + self.role_function.KHQ + self.role_function.HQ)
+
+    def __str__(self):
+        return "{:.2f}/10".format(self.investment_index) if self.investment_index else "unknown"
---
+++ main.py
@@ -0,0 +1,70 @@
+from fuzzy import *
+import tkinter
+
+def is_decimal(num):
+    try:
+        return num.replace('.', '', 1).isnumeric()
+    except:
+        return num.isnumeric()
+    
+def calculate_fuzzy():
+    _profit = profitEntry.get()
+    _cost = costEntry.get()
+    _time = timeEntry.get()
+
+    if _profit and _cost and _time:
+        if is_decimal(_profit) and is_decimal(_cost) and is_decimal(_time):
+            print(_profit, _cost, _time)
+
+            my_fuzzy = Fuzzy(float(_profit), float(_cost), float(_time))
+            my_fuzzy.inference()
+            my_fuzzy.defuzzifier()
+
+            errorLabel.config(text="")
+            resultLabel.config(text=f"Hiệu quả đầu tư: {my_fuzzy}")
+            print(my_fuzzy)
+        else:
+            errorLabel.config(text="Một trong ba giá trị không phải là số")
+    else:
+        errorLabel.config(text="Bạn cần phải nhập thông tin trước khi xem kết quả")
+
+
+
+
+
+window = tkinter.Tk()
+window.title("Đánh giá hiệu quả đầu tư")
+
+frame = tkinter.Frame(window)
+frame.pack()
+
+fuzzyFrame = tkinter.LabelFrame(frame, text="Hiệu quả đầu tư")
+fuzzyFrame.grid(row=0, column=0, padx=20, pady=20)
+
+profitLabel = tkinter.Label(fuzzyFrame, text="Tỷ suất lợi nhuận")
+profitLabel.grid(row=0, column=0)
+
+costLabel = tkinter.Label(fuzzyFrame, text="Chi phí đầu tư")
+costLabel.grid(row=0, column=1)
+
+timeLabel = tkinter.Label(fuzzyFrame, text="Thời gian hoàn vốn")
+timeLabel.grid(row=0, column=2)
+
+profitEntry = tkinter.Entry(fuzzyFrame)
+costEntry = tkinter.Entry(fuzzyFrame)
+timeEntry = tkinter.Entry(fuzzyFrame)
+
+profitEntry.grid(row=1, column=0)
+costEntry.grid(row=1, column=1)
+timeEntry.grid(row=1, column=2)
+
+resultLabel = tkinter.Label(frame)
+resultLabel.grid(row=1, column=0)
+
+errorLabel = tkinter.Label(frame)
+errorLabel.grid(row=2, column=0)
+
+button = tkinter.Button(frame, text="Xem kết quả", command=calculate_fuzzy)
+button.grid(row=3, column=0, sticky="news", padx=20, pady=20)
+
+window.mainloop()
